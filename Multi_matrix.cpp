52e0cdfd2d25f5e82b5f9952f27758006c5c1468
serradur4 <155892875+serradur4@users.noreply.github.com>
2024-02-16 12:32:00 +0100

    Update Multi_matrix.cpp

--- Multi_matrix.cpp
+++ Multi_matrix.cpp
@@ -1,16 +1,19 @@
-void product_vector_vet(const vector<vector<double>>& mat1, const vector<vector<double>>& mat2, vector<vector<double>>& mat3) {
+#include <iostream>
+#include <vector>
+using namespace std;
+void  multi_matrix(const vector<vector<double>>& mat1, const vector<vector<double>>& mat2, vector<vector<double>>& mat3) {
 	for (int i = 0; i < mat3.size(); i++) {
 		for (int j = 0; j < mat3.at(i).size(); j++) {
 			double sum_i_j = 0;
 			for (int k = 0; k < mat2.size(); k++) {
-				sum_i_j += mat1.at(i).at(k) * mat2.at(k).at(j);  // mat1.at(i).at(k) è come scrivere mat1[i][j] (solo che at ti restituisce il controllo) 
+				sum_i_j += mat1.at(i).at(k) * mat2.at(k).at(j);  
 			}
 			mat3.at(i).at(j) = sum_i_j;
 		}
 	}
 }
-void print_matrix_vet(const vector<vector<double>>& mat) {
-	for (vector<double> row : mat) {    // for each che stampa gli elementi di una matrice 
+void print_multi_matrix(const vector<vector<double>>& mat) {
+	for (vector<double> row : mat) {    		// for each which prints the elements of a matrix
 		for (double item : row) {
 			cout << item << " ";
 		}
@@ -19,20 +22,19 @@ void print_matrix_vet(const vector<vector<double>>& mat) {
 	cout << "\n\n";
 } 
 
-int main() {
+int main(int argc, char* argv []) {
 	int matrix1[2][3] = { {2, 3, 4}, {1, 2, 3} };
 	int matrix2[3][4] = { {1, 2, 3, 4}, {9, 0, 7, 7}, {2, 3, 9, 7} };
 	int matrix3[2][4] = {};
-	product_matrix(matrix1, matrix2, matrix3);
-	print_matrix(matrix3, 2);
+	multi_matrix(matrix1, matrix2, matrix3);
+	print_multi_matrix(matrix3, 2);
 	
+	vector<vector<double>> mat1 = { {1, 2, 3}, {7, 8, 9}, {0, 2, 3}, {1, 1, 2} }; 			// matrix of vector 4 x 3 
+	vector<vector<double>> mat2 = { {0, 0, 9, 0, 9}, {2.3, 4, 5, 0, 8}, {1, 2, 4, 9, 7} };  			// matrix of vector 3 x 5 
+	vector<vector<double>> mat3 = { {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0} };  				// matrix of vector4 x 5
 
-	vector<vector<double>> mat1 = { {1, 2, 3}, {7, 8, 9}, {0, 2, 3}, {1, 1, 2} }; // matrice di vector 4 x 3 
-	vector<vector<double>> mat2 = { {0, 0, 9, 0, 9}, {2.3, 4, 5, 0, 8}, {1, 2, 4, 9, 7} };  // matrice di vector 3 x 5 
-	vector<vector<double>> mat3 = { {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0} };  // matrice di vector 4 x 5
-
-	product_vector_vet(mat1, mat2, mat3);
-	print_matrix_vet(mat3);
+	multi_matrix(mat1, mat2, mat3);
+	print_multi_matrix(mat3);
 
 
 }
